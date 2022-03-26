1fd0a854c7f43d9551172e4d973345855caf1392
KEERTHANA-E <ekeerthana2020@gmail.com>
2022-03-26 16:31:44 +0530

    Added About Menu in Application

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import "./App.css";
 import { Route, Routes } from 'react-router-dom'
 import Home from "./components/Home";
 import NavBar from "./components/NavBar";
-import Joinus from "./components/Joinus"
-
+import Joinus from "./components/Joinus";
+import About from "./components/About";
+import Support from "./components/Faq";
 function App() {
   return (
     <div>
@@ -12,6 +13,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/joinus" element={<Joinus />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/support" element={<Support />}></Route>
       </Routes>
     </div>
   )
---
+++ src/components/Faq.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./Faq.css";
+function Faq() {
+  return (
+    <div>
+      <h1>FAQ'S</h1>
+    </div>
+  )
+}
+
+export default Faq
